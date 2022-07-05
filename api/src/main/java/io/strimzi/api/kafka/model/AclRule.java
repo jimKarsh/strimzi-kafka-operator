093189b756fe2b6f108c41be96c7315cf31f0fdf
Andie Hegarty <AndieHegarty@andies-mbp.mynetworksettings.com>
2022-07-05 11:56:29 -0400

    Add operations configuration option, deprecate operation property, serialize to options property by default
    Signed-off-by: James Karsh <james.karsh@gmail.com>

--- api/src/main/java/io/strimzi/api/kafka/model/AclRule.java
+++ api/src/main/java/io/strimzi/api/kafka/model/AclRule.java
@@ -4,16 +4,22 @@
  */
 package io.strimzi.api.kafka.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.strimzi.api.annotations.DeprecatedProperty;
 import io.strimzi.crdgenerator.annotations.Description;
 import io.strimzi.crdgenerator.annotations.DescriptionFile;
+import io.strimzi.crdgenerator.annotations.OneOf;
 import io.sundr.builder.annotations.Buildable;
 import io.vertx.core.cli.annotations.DefaultValue;
 import lombok.EqualsAndHashCode;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.EnumSet;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -24,6 +30,7 @@ import java.util.Map;
         editableEnabled = false,
         builderPackage = Constants.FABRIC8_KUBERNETES_API
 )
+@OneOf({@OneOf.Alternative(@OneOf.Alternative.Property("operation")), @OneOf.Alternative(@OneOf.Alternative.Property("operations"))})
 @JsonInclude(JsonInclude.Include.NON_DEFAULT)
 @EqualsAndHashCode
 public class AclRule implements UnknownPropertyPreserving, Serializable {
@@ -34,16 +41,22 @@ public class AclRule implements UnknownPropertyPreserving, Serializable {
     private AclRuleResource resource;
     private String host = "*";
     private AclOperation operation;
+    private List<AclOperation> operations = new ArrayList<>(0);
     private Map<String, Object> additionalProperties = new HashMap<>(0);
 
     public AclRule() {
     }
 
     public AclRule(AclRuleType type, AclRuleResource resource, String host, AclOperation operation) {
+        this(type, resource, host, List.of(operation));
+        this.operation = operation;
+    }
+
+    public AclRule(AclRuleType type, AclRuleResource resource, String host, List<AclOperation> operations) {
         this.type = type;
         this.resource = resource;
         this.host = host;
-        this.operation = operation;
+        this.operations.addAll(operations);
     }
 
     @Description("The type of the rule. " +
@@ -81,15 +94,30 @@ public class AclRule implements UnknownPropertyPreserving, Serializable {
         this.host = host;
     }
 
+    @Deprecated
+    @DeprecatedProperty(description = "\"operation\" deprecated in favor of \"operations\": []", movedToPath = "operations: []")
     @Description("Operation which will be allowed or denied. " +
             "Supported operations are: Read, Write, Create, Delete, Alter, Describe, ClusterAction, AlterConfigs, DescribeConfigs, IdempotentWrite and All.")
-    @JsonProperty(required = true)
+    @JsonIgnore
     public AclOperation getOperation() {
-        return operation;
+        return this.operation;
     }
 
+    @JsonProperty("operation")
     public void setOperation(AclOperation operation) {
         this.operation = operation;
+        this.operations.add(operation);
+    }
+
+    @Description("Operation(s) which will be allowed or denied. " +
+            "Supported operations are: Read, Write, Create, Delete, Alter, Describe, ClusterAction, AlterConfigs, DescribeConfigs, IdempotentWrite and All.")
+    public List<AclOperation> getOperations() {
+        return this.operations.isEmpty() ? List.of() : List.copyOf(EnumSet.copyOf(this.operations));
+//        return this.operations.stream().distinct().sorted(AclOperation::compareTo).collect(Collectors.toUnmodifiableList());
+    }
+
+    public void setOperations(List<AclOperation> operations) {
+        this.operations.addAll(operations);
     }
 
     @Override
@@ -101,4 +129,5 @@ public class AclRule implements UnknownPropertyPreserving, Serializable {
     public void setAdditionalProperty(String name, Object value) {
         this.additionalProperties.put(name, value);
     }
+
 }
